ca5c50ecc7e6cdc45d70be4ae4841eff33c6ee86
Mostafa-em <mostafaemamian73@gmail.com>
2019-02-16 19:23:28 +0330

    fist day

---
+++ app/src/main/java/com/example/list_view_pro/list_Item_model.java
@@ -0,0 +1,44 @@
+package com.example.list_view_pro;
+
+public class list_Item_model {
+    private int img;
+    private String title;
+    private String auther;
+    private int pageCount;
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuther() {
+        return auther;
+    }
+
+    public void setAuther(String auther) {
+        this.auther = auther;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+
+
+
+
+}
